78c52c8d9672d9a634cddb2908909c01a80ea800
andrea estevao <andestevao@gmail.com>
2026-05-13 18:24:31 -0300

    checkpoint

--- etl/ml/train_model.py
+++ etl/ml/train_model.py
@@ -1,4 +1,4 @@
-from time import timezone
+from datetime import timezone, datetime
 
 from airflow.providers.postgres.hooks.postgres import (
     PostgresHook,
@@ -12,6 +12,7 @@ import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import (
     mean_absolute_error,
+
     mean_squared_error,
     r2_score,
 )
